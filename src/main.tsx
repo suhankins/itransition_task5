8abb33127f550b80bfc6fd8d46f01df7859f4358
suhankins <punishedbernadetta@niuitmo.ru>
2023-03-07 23:20:05 +0400

    removed pico.min.css, replaced it with npm package

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import './pico.min.css';
+import '@picocss/pico/css/pico.css';
 import './custom.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
